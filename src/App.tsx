008cd72d963a11ab79734c5647084bae553c84dd
femolos <oluwafemiolosunde@gmail.com>
2026-06-29 18:15:17 -0400

    add sort books function

--- src/App.tsx
+++ src/App.tsx
@@ -5,3 +5,13 @@ import SearchBar from './components/SearchBar';
 import BookCard from './components/BookCard';
 import StatsBar from './components/StatsBar';
 import LibraryControls from './components/LibraryControls';
+
+function sortBooks(books: Book[], field: SortField, dir: SortDir): Book[] {
+  return [...books].sort((a, b) => {
+    let cmp = 0;
+    if (field === 'title') cmp = a.title.localeCompare(b.title);
+    else if (field === 'author') cmp = a.author.localeCompare(b.author);
+    else cmp = a.addedAt - b.addedAt;
+    return dir === 'asc' ? cmp : -cmp;
+  });
+}
